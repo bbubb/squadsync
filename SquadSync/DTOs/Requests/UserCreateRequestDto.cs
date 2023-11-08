db38372d367700106a298894eef81e96728b8d70
bbubb <brandoncbunch@gmail.com>
2023-11-07 19:56:09 -0600

    Corrected UserCreate Dto and Validator type

--- SquadSync/DTOs/Requests/UserCreateRequestDto.cs
+++ SquadSync/DTOs/Requests/UserCreateRequestDto.cs
@@ -2,5 +2,13 @@
 {
     public class UserCreateRequestDto
     {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        //public RoleDto RoleDto { get; set; }
     }
 }
