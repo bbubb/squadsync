345029eae997955e902e97b17bb7e7f1577133df
bbubb <brandoncbunch@gmail.com>
2023-11-16 00:54:38 -0600

    Added CRUD to RoleRepository and changed User ModifiedOn

--- SquadSync/Data/Repositories/IRepositories/IRoleRepository.cs
+++ SquadSync/Data/Repositories/IRepositories/IRoleRepository.cs
@@ -7,11 +7,14 @@ namespace SquadSync.Data.Repositories.IRepositories
 {
     public interface IRoleRepository
     {
-        Task<IEnumerable<User>> GetUsersByRoleAsync(RoleNameEnum role);
-        Task<IEnumerable<Role>> GetRolesByUserGuidAsync (Guid guid);
-        Task AssignRoleToUserAsync(Guid guid, RoleCreateRequestDto roleCreateRequestDto);
-        Task RemoveRoleFromUserAsync(Guid guid, Guid roleGuid);
         Task<IEnumerable<Role>> GetAllRolesAsync();
+        Task<IEnumerable<User>> GetUsersByRoleAsync(RoleNameEnum role);
         Task<IEnumerable<User>> GetUserWithMultipleRolesAsync(RoleNameEnum[] roles);
+        Task<IEnumerable<Role>> GetRolesByUserGuidAsync (Guid guid);
+        Task<Role> GetRoleByGuidAsync(Guid guid);
+        Task CreateRoleAsync(Role role);
+        Task UpdateRoleAsync(Role role);
+        Task ArchiveRoleAsync(Role role);
+        Task DeleteRoleAsync(Role role);
     }
 }
