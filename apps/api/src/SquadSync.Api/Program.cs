f95bad341c3649e44c40667cfcbc7f761fe856c8
bbubb <brandoncbunch@gmail.com>
2026-09-23 11:55:33 -0500

    Add health endpoint and development Swagger for issue #45

--- apps/api/src/SquadSync.Api/Program.cs
+++ apps/api/src/SquadSync.Api/Program.cs
@@ -1,9 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSwaggerGen();
+}
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapHealthChecks("/health");
 app.MapControllers();
 
 app.Run();
